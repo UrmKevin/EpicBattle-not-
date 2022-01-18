aadc613fcbded8432b781c4a5faeeb3e57f368b3
KevinUrm <kevinurm2005@gmail.com>
2022-01-18 13:34:39 +0200

    Upadate*

--- EpicBattle(not)/Program.cs
+++ EpicBattle(not)/Program.cs
@@ -12,19 +12,43 @@ namespace EpicBattle_not_
             string[] heroes = GetDataFromFile(rootPath + "heroes.txt");
             string[] villains = GetDataFromFile(rootPath + "villains.txt");
             string[] weapons = GetDataFromFile(rootPath + "weapons.txt");
+            string[] armor = GetDataFromFile(rootPath + "armor.txt");
 
-            string randomHero = GetRandomCharacter(heroes);
-            string randomVillain = GetRandomCharacter(villains);
+            string randomHero = GetRandomElement(heroes);
+            string randomVillain = GetRandomElement(villains);
             string randomHeroWeapon = GetWeapon(weapons);
             string randomVillainWeapon = GetWeapon(weapons);
+            string heroArmor = GetRandomElement(armor);
+            string villainArmor = GetRandomElement(armor);
+            int heroHP = GenerateHP(heroArmor);
+            int villainHP = GenerateHP(villainArmor);
 
-            Console.WriteLine($"UR HERO IS {randomHero}");
-            Console.WriteLine($"with {randomHeroWeapon}");
-            Console.WriteLine($"UR VILLIAN IS {randomVillain}");
-            Console.WriteLine($"with {randomVillainWeapon}");
+
+            Console.WriteLine($"{randomHero} in {heroArmor} with {heroHP} HP");
+            Console.WriteLine("             V/S             ");
+            Console.WriteLine($"{randomVillain} in {villainArmor} with {villainHP} HP");
+
+            while (heroHP >= 0 && villainHP >= 0)
+            {
+                heroHP = heroHP - Hit(randomVillain, randomVillainWeapon);
+                villainHP = villainHP - Hit(randomHero, randomHeroWeapon);
+            }
+
+            if(heroHP > villainHP)
+            {
+                Console.WriteLine($"{randomHero} saves the day!");
+            } else if(heroHP < villainHP)
+            {
+                Console.WriteLine($"Dark side wins!");
+            }
+            else
+            {
+                Console.WriteLine($"Both {randomHero} and {randomVillain} dropped dead.");
+            }
+
         }
 
-        public static string GetRandomCharacter(string[] someArray)
+        public static string GetRandomElement(string[] someArray)
         {
             return someArray[GetRandomIndex(someArray)];
         }
@@ -44,6 +68,28 @@ namespace EpicBattle_not_
         {
             string[] dataFromFile = File.ReadAllLines(filePath);
             return dataFromFile;
+        }
+
+        public static int GenerateHP(string armor)
+        {
+            return armor.Length;
+        }
+
+        public static int Hit(string character, string weapon)
+        {
+            Random rnd = new Random();
+            int strike = rnd.Next(0, weapon.Length - 2);
+            Console.WriteLine($"{character} hit {strike}.");
+
+            if (strike==0)
+            {
+                Console.WriteLine($"Bad luck. {character} missed the target");
+            } else if(strike == weapon.Length - 2)
+            {
+                Console.WriteLine($"Awesome! {character} made a critical hit!");
+            }
+
+            return strike;
         }
 
     }
